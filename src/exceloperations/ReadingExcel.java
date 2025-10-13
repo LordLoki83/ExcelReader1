17b46f31e6d7866214f2569ffe420a3f1fff97e7
Lukas Hilzensauer <lukas.hilzensauer@gmx.at>
2025-10-13 11:47:04 +0200

    Erste Änderungen

---
+++ src/exceloperations/ReadingExcel.java
@@ -0,0 +1,46 @@
+package exceloperations;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Date;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.xssf.usermodel.*;
+
+public class ReadingExcel {
+    public static void main(String[] args) throws IOException {
+        
+        String excelFilePath = "/Users/lukas/Documents/ExcelReader2/ExcelReader2/Datafiles/AbstellungenKW42.xlsx";
+        FileInputStream inputstream = new FileInputStream(excelFilePath);
+
+        XSSFWorkbook workbook = new XSSFWorkbook(inputstream); 
+        XSSFSheet sheet =  workbook.getSheetAt(0);
+
+        int rows = sheet.getLastRowNum();
+        int cols = sheet.getRow(6).getLastCellNum();
+
+        Object loken[][] = new Object[rows-5][cols];
+        int i = 0;
+        for( int r = 6 ; r <= rows; r++){
+            XSSFRow row = sheet.getRow(r);
+            XSSFCell cell = row.getCell(2);
+            if(!cell.getStringCellValue().contains("1142")& !cell.getStringCellValue().contains("1163")& !cell.getStringCellValue().contains("1064")& !cell.getStringCellValue().contains("1063")){
+                for(int c = 0; c < cols; c++){
+                    XSSFCell cell1 = row.getCell(c); 
+                    if (cell1.getCellType()== CellType.STRING){
+                        String text = cell1.getStringCellValue();
+                        loken[i][c] = text;
+                        System.out.print("" + loken[i][c]+ " ");
+                    } else if (cell1.getCellType()== CellType.NUMERIC){
+                        Date text = cell1.getDateCellValue();
+                        loken[i][c] = text;
+                        System.out.print("" + loken[i][c]+ " ");
+                    }     
+                }
+                i++;
+                System.out.println();
+            } 
+            
+        }
+        System.out.println(i);
+    }
+}
---
+++ src/exceloperations/Sortieren.java
@@ -0,0 +1,36 @@
+package exceloperations;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Sortieren {
+ 
+    static void SortierenLoken(Object loken[][] ){
+        Object loken2[][] = new Object[loken.length][loken[1].length];
+        int i = 0;
+        
+        for(int f = 0; f <loken.length;f++ ){
+        List<Object> list = Arrays.asList(loken[f]);    
+        if(list.get(2).toString().contains("1016")||list.get(2).toString().contains("1116")|| list.get(2).toString().contains("1144")|| list.get(2).toString().contains("1216")|| list.get(2).toString().contains("1293")){
+                    
+           for(int j = 0; j < loken[f].length;j++ ){
+                loken2[i][j]= list.get(j);
+                    System.out.print("" + loken2[i][j]+ " ");
+                    
+                }
+                i++;
+            System.out.println();
+            }
+        }
+    }
+
+    static void DatumAbgleich(Object loken[][]){
+        /*
+        for-Schleife von 0 bis Zeilen-Ende (i)
+            loken[0][i] in ein zweites Array loken2[0][i]übertragen
+
+            
+         */
+
+    }
+}
