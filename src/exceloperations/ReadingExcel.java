e033c121b16aa91f77950492a01010d806ed3fad
Lukas Hilzensauer <lukas.hilzensauer@gmx.at>
2025-10-18 15:29:38 +0200

    Umbau der Main-Methode Vers1

--- src/exceloperations/ReadingExcel.java
+++ src/exceloperations/ReadingExcel.java
@@ -1,65 +1,35 @@
 package exceloperations;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import org.apache.poi.ss.usermodel.CellType;
-import org.apache.poi.xssf.usermodel.*;
-
 public class ReadingExcel {
     public static void main(String[] args) throws IOException, ParseException {
-        
-        String excelFilePath = "/Users/lukas/Documents/ExcelReader2/ExcelReader2/Datafiles/AbstellungenKW42.xlsx";
-        FileInputStream inputstream = new FileInputStream(excelFilePath);
+        int c= 0;
+        Object loken2[][]= (Object[][]) Sortieren.DatumAbgleich();
+        String dateStr3 = "22-10-2025";     //Das Datum der aktiven Datensätze einlesen
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        Date monday = sdf.parse(dateStr3);
 
-        XSSFWorkbook workbook = new XSSFWorkbook(inputstream); 
-        XSSFSheet sheet =  workbook.getSheetAt(0);
 
-        int rows = sheet.getLastRowNum();
-        int cols = sheet.getRow(5).getLastCellNum();
-
-        Object loken[][] = new Object[rows-5][cols];
-        int i = 0;
-
-        for( int r = 6 ; r <= rows; r++){
-            XSSFRow row = sheet.getRow(r);
-            XSSFCell cellLok = row.getCell(2);
-            XSSFCell cellArt = row.getCell(5);
-
-            if(!cellArt.getStringCellValue().contains("aREP")){
-
-                if(!cellLok.getStringCellValue().contains("1142")& !cellLok.getStringCellValue().contains("1163")& !cellLok.getStringCellValue().contains("1064")& !cellLok.getStringCellValue().contains("1063")){
-
-                    for(int c = 0; c < cols; c++){
-                        XSSFCell cell1 = row.getCell(c); 
-
-                        if (cell1.getCellType()== CellType.STRING){
-                            String text = cell1.getStringCellValue();
-                            loken[i][c] = text;
-
-                        } else if (cell1.getCellType()== CellType.NUMERIC){
-                            Date text = cell1.getDateCellValue();
-                            SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");   //Korrektur Format Datum
-                            String datum = format.format(text);
-                            loken[i][c] = datum;
-                        }  
-
-                    }
-                    i++;
-                }  
-            } 
-        }
-        Object loken3[][]= (Object[][]) Sortieren.DatumAbgleich(loken);
-       
         for (int a = 0; a < Sortieren.Ruckgabe(); a++ ){
-            for (int b = 0; b < loken3[0].length; b++ ){
-                System.out.print("" + loken3[a][b] + " ");
+            String start = (String) loken2[a][3];
+            String end = (String) loken2[a][4];
+            
+            Date startZeit = sdf.parse(start);
+            Date endZeit = sdf.parse(end);
+            if(!monday.before(startZeit) && !monday.after(endZeit)){
+                for (int b = 0; b < loken2[0].length; b++ ){
+                    System.out.print("" + loken2[a][b] + " ");
+                }
+                c++;
+                System.out.println(); 
+            }
+           
+         
         }
-         System.out.println(); 
-        }
-        System.out.print(Sortieren.Ruckgabe());
+        System.out.print(c);
     }
 }
--- src/exceloperations/Sortieren.java
+++ src/exceloperations/Sortieren.java
@@ -2,27 +2,85 @@ package exceloperations;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.text.ParseException;
 public class Sortieren {
     static int h = 0;
-    static Object DatumAbgleich(Object loken[][]) throws ParseException{
-        Object loken2[][] = new Object[loken.length][8];
+
+    static Object ExcelSort() throws IOException{
+        
+        String excelFilePath = "/Users/lukas/Documents/ExcelReader2/ExcelReader2/Datafiles/AbstellungenKW43.xlsx"; //Zellverbund muss aufgehoben werden
+        FileInputStream inputstream = new FileInputStream(excelFilePath);
+        XSSFWorkbook workbook = new XSSFWorkbook(inputstream); 
+        XSSFSheet sheet =  workbook.getSheetAt(0);
+
+        int rows = sheet.getLastRowNum();
+        int cols = sheet.getRow(5).getLastCellNum();
+
+        Object loken[][] = new Object[rows-5][cols];
+        int j = 0;
+        for( int r = 6 ; r <= rows; r++){
+            XSSFRow row = sheet.getRow(r);
+            XSSFCell cellReg = row.getCell(0);
+            XSSFCell cellLok = row.getCell(2);
+            XSSFCell cellArt = row.getCell(5);
+
+            if(!cellReg.getStringCellValue().contains("GBL")){
+                if(!cellArt.getStringCellValue().contains("aREP")& !cellArt.getStringCellValue().contains("SFPR")&!cellArt.getStringCellValue().contains("SFTS")){
+
+                    if(!cellLok.getStringCellValue().contains("1142")& !cellLok.getStringCellValue().contains("1163")& !cellLok.getStringCellValue().contains("1064")& !cellLok.getStringCellValue().contains("1063")){
+
+                        for(int c = 0; c < cols; c++){
+                            XSSFCell cell1 = row.getCell(c); 
+
+                            if (cell1.getCellType()== CellType.STRING){
+                                String text = cell1.getStringCellValue();
+                                loken[j][c] = text;
+
+                            } else if (cell1.getCellType()== CellType.NUMERIC){
+                                Date text = cell1.getDateCellValue();
+                                SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");   //Korrektur Format Datum
+                                String datum = format.format(text);
+                                loken[j][c] = datum;
+                            }  
+
+                        }
+                    }
+                        j++;
+                }  
+            } 
+        }
+        return loken;
+    }
+
+    
+    static Object DatumAbgleich() throws ParseException, IOException{
+        Object loken1[][]= (Object[][]) ExcelSort();
+        Object loken2[][] = new Object[loken1.length][8];
         int b = 0;
         //Die erste Stelle in Loken2 befüllen, wenn diese leer ist
         if (loken2[0][0]== null){
-            loken2[0][0] = loken[0][0];
-            loken2[0][1] = loken[0][1];
-            loken2[0][2] = loken[0][2];
-            loken2[0][3] = loken[0][3];
-            loken2[0][4] = loken[0][4];
-            loken2[0][5] = loken[0][5];
-            loken2[0][7] = loken[0][7];
+            loken2[0][0] = loken1[0][0];
+            loken2[0][1] = loken1[0][1];
+            loken2[0][2] = loken1[0][2];
+            loken2[0][3] = loken1[0][3];
+            loken2[0][4] = loken1[0][4];
+            loken2[0][5] = loken1[0][5];
+            loken2[0][7] = loken1[0][7];
             h++;
             }
         //Prüfen, ob Loken1 gleich Loken2 ist    
         for (int i = 0; i < loken2.length; i++){
-            String dateStr = (String) loken[i][3];       //Das Datum der aktiven Datensätze einlesen
-            String dateStr1 = (String) loken[i][4];      //Das Datum der aktiven Datensätze einlesen
+            String dateStr = (String) loken1[i][3];       //Das Datum der aktiven Datensätze einlesen
+            String dateStr1 = (String) loken1[i][4];      //Das Datum der aktiven Datensätze einlesen
             String dateStr2 = (String) loken2[b][3];     //Das Datum der aktiven Datensätze einlesen
             String dateStr3 = (String) loken2[b][4];     //Das Datum der aktiven Datensätze einlesen
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
@@ -33,37 +91,34 @@ public class Sortieren {
                 Date lok2Date1 = sdf.parse(dateStr2);       //Umwandeln der Datum-Strings in Date-Datensätze
                 Date lok2Date2 = sdf.parse(dateStr3);
 
-                if (loken[i][2] != loken2[b][2]){           //Prüfen, ob die Loknummer in Array 2 ungleich zu Array 1 sind
+                if(loken1[i][2] == loken2[b][2] & !lok1Date1.equals(lok2Date1) & !lok1Date2.equals(lok2Date2) ) {           //Prüfen, ob die Loknummer in Array 2 ungleich zu Array 1 sind
                     b++;
-                    loken2[b][0] = loken[i][0];
-                    loken2[b][1] = loken[i][1];
-                    loken2[b][2] = loken[i][2];
-                    loken2[b][3] = loken[i][3];
-                    loken2[b][4] = loken[i][4];
-                    loken2[b][5] = loken[i][5];
-                    loken2[b][7] = loken[i][7];
+                    loken2[b][0] = loken1[i][0];
+                    loken2[b][1] = loken1[i][1];
+                    loken2[b][2] = loken1[i][2];
+                    loken2[b][3] = loken1[i][3];
+                    loken2[b][4] = loken1[i][4];
+                    loken2[b][5] = loken1[i][5];
+                    loken2[b][7] = loken1[i][7];
                     for (int a = 0; a < loken2[0].length; a++){
                     }   
-
                     h++;
                 //Wenn die Loknummer gleich ist, aber das Datum unterschiedlich
-                }else if(loken[i][2] == loken2[b][2] & !lok1Date1.equals(lok2Date1) & !lok1Date2.equals(lok2Date2) ) {
+                }else if(loken1[i][2] != loken2[b][2]){
                     b++;
-                    loken2[b][0] = loken[i][0];
-                    loken2[b][1] = loken[i][1];
-                    loken2[b][2] = loken[i][2];
-                    loken2[b][3] = loken[i][3];
-                    loken2[b][4] = loken[i][4];
-                    loken2[b][5] = loken[i][5];
-                    loken2[b][7] = loken[i][7];
+                    loken2[b][0] = loken1[i][0];
+                    loken2[b][1] = loken1[i][1];
+                    loken2[b][2] = loken1[i][2];
+                    loken2[b][3] = loken1[i][3];
+                    loken2[b][4] = loken1[i][4];
+                    loken2[b][5] = loken1[i][5];
+                    loken2[b][7] = loken1[i][7];
                     for (int a = 0; a < loken2[0].length; a++){
                     } 
                     h++;
                 }
             }else{
-
             }
-            
         }
     return loken2 ;
     }
@@ -71,6 +126,5 @@ public class Sortieren {
     static int Ruckgabe(){
 
         return h;
-
     }
 }
